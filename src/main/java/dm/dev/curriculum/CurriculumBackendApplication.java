18772899b36dd506d14b8201325f8c7ac6baa54a
d.morgunov <d.morgunov@huskyjam.co>
2021-05-17 18:19:37 +0300

    Init

---
+++ src/main/java/dm/dev/curriculum/CurriculumBackendApplication.java
@@ -0,0 +1,13 @@
+package dm.dev.curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dm/dev/curriculum/CurriculumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.dev.curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
